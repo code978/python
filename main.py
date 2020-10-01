01bfb83c77e205248d2e7cb02b840a3b5ca8087c
code978 <56649142+code978@users.noreply.github.com>
2020-10-01 09:43:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+# Python Program to Generate a Random Number
+# In this example, you will learn to generate a random number in Python.
+
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+
+print(random.randint(0,9))
