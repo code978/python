03cef02bd78d5f82cc18e43f1cab48e529d2faca
code978 <56649142+code978@users.noreply.github.com>
2020-10-01 09:45:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hello World")
+
+# Python Program to Convert Kilometers to Miles
+# In this example, we'll learn to convert kilometers to miles and display it.
+
+# Taking kilometers input from the user
+kilometers = float(input("Enter value in kilometers: "))
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
